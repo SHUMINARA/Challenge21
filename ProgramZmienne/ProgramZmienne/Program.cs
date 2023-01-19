617d1ac9537dd7f7786b49af7d29466114112c17
P S Rozycki <sean.ritter@outlook.com>
2023-01-19 13:04:28 +0000

    Working with variables

---
+++ ProgramZmienne/ProgramZmienne/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+string name, ageString, genderString;
+int age;
+Console.WriteLine("Please enter your name?");
+name = Console.ReadLine();
+bool ewa = name == "ewa";
+Console.WriteLine("Please enter your age?");
+ageString = Console.ReadLine();
+age = int.Parse(ageString);
+Console.WriteLine("Please enter your gender (m/f)?");
+genderString = Console.ReadLine();
+bool woman = genderString == "f";
+
+if (age < 30 && woman)
+{
+    Console.WriteLine("\nYou are woman under 30 years old.");
+}
+else if (ewa && age is 33)
+{
+    Console.WriteLine("\nYou are Ewa and you are 33 years old.");
+}
+else if (!woman && age < 18)
+{
+    Console.WriteLine("\nYou are underage man.");
+}
+else
+{ Console.WriteLine("\nI have no idea who are you :)"); }
